dfcd6defede5091f51137de53ee7ca458f758dc0
Stanislau Wolski <stanislau.wolski@gmail.com>
2013-09-04 14:22:46 +0300

    [add] initial version

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ node-scheduler-demo
 ===================
 
 Demo of dhtmlxScheduler with NodeJs + MongoDB as backend
+
+### How to start
+
+To run the app, do the next after cloning the repo
+
+~~~
+npm install
+node app.js
+~~~
+
+after that, open in a browser http://localhost:3000/init to generate a test data, and http://localhost:3000 to check the calendar. 
+
+### DB config
+
+App expects to find the mongoDB on localhost, you can change the server address in the app.js 
---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var path = require('path');
+
+var db = require('mongoskin').db("localhost/testdb", { w: 0});
+    db.bind('event');
+
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
+
+
+app.get('/init', function(req, res){
+	db.event.insert({ 
+		text:"My test event A", 
+		start_date: new Date(2013,8,1),
+		end_date:	new Date(2013,8,5)
+	});
+	db.event.insert({ 
+		text:"One more test event", 
+		start_date: new Date(2013,8,3),
+		end_date:	new Date(2013,8,8),
+		color: "#DD8616"
+	});
+
+	res.send("Test events were added to the database")
+});
+
+
+app.get('/data', function(req, res){
+	db.event.find().toArray(function(err, data){
+		//set id property for all records
+		for (var i = 0; i < data.length; i++)
+			data[i].id = data[i]._id;
+		
+		//output response
+		res.send(data);
+	});
+});
+
+
+app.post('/data', function(req, res){
+	var data = req.body;
+	var mode = data["!nativeeditor_status"];
+	var sid = data.id || data.gr_id; //for grid
+	var tid = sid;
+
+	delete data.id;
+	delete data.gr_id;
+	delete data["!nativeeditor_status"];
+
+
+	function update_response(err, result){
+		if (err)
+			mode = "error";
+		else if (mode == "inserted")
+			tid = data._id;
+
+		res.setHeader("Content-Type","text/xml");
+		res.send("<data><action type='"+mode+"' sid='"+sid+"' tid='"+tid+"'/></data>");
+	}
+
+	if (mode == "updated")
+		db.event.updateById( sid, data, update_response);
+	else if (mode == "inserted")
+		db.event.insert(data, update_response);
+	else if (mode == "deleted")
+		db.event.removeById( sid, update_response);
+	else
+		res.send("Not supported operation");
+});
+
+app.listen(3000);
