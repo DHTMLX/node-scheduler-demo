a47e42f5f28017f504307423dd9693c71d828010
Stanislau Wolski <stanislau.wolski@gmail.com>
2013-10-29 12:33:34 +0300

    [add] example of usage with backbone

--- app.js
+++ app.js
@@ -10,74 +10,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
 
 
-app.get('/init', function(req, res){
-	db.event.insert({ 
-		text:"My test event A", 
-		start_date: new Date(2013,8,1),
-		end_date:	new Date(2013,8,5)
-	});
-	db.event.insert({ 
-		text:"My test event B", 
-		start_date: new Date(2013,8,19),
-		end_date:	new Date(2013,8,24)
-	});
-	db.event.insert({ 
-		text:"Morning event", 
-		start_date: new Date(2013,8,4,4,0),
-		end_date:	new Date(2013,8,4,14,0)
-	});
-	db.event.insert({ 
-		text:"One more test event", 
-		start_date: new Date(2013,8,3),
-		end_date:	new Date(2013,8,8),
-		color: "#DD8616"
-	});
 
-	res.send("Test events were added to the database")
-});
-
-
-app.get('/data', function(req, res){
+app.get('/events', function(req, res){
 	db.event.find().toArray(function(err, data){
-		//set id property for all records
-		for (var i = 0; i < data.length; i++)
-			data[i].id = data[i]._id;
-		
-		//output response
-		res.send(data);
+		if (err) res.send("error");
+		else res.send(data);
 	});
 });
 
-
-app.post('/data', function(req, res){
-	var data = req.body;
-	var mode = data["!nativeeditor_status"];
-	var sid = data.id;
-	var tid = sid;
-
-	delete data.id;
-	delete data.gr_id;
-	delete data["!nativeeditor_status"];
-
-
-	function update_response(err, result){
-		if (err)
-			mode = "error";
-		else if (mode == "inserted")
-			tid = data._id;
-
-		res.setHeader("Content-Type","text/xml");
-		res.send("<data><action type='"+mode+"' sid='"+sid+"' tid='"+tid+"'/></data>");
-	}
-
-	if (mode == "updated")
-		db.event.updateById( sid, data, update_response);
-	else if (mode == "inserted")
-		db.event.insert(data, update_response);
-	else if (mode == "deleted")
-		db.event.removeById( sid, update_response);
-	else
-		res.send("Not supported operation");
+app.post('/events',function(req, res){
+	db.event.insert(req.body, function(err, result){
+		if (err) res.send("error");
+		else res.send(result[0]);
+	});
 });
+
+app.put('/events/:id', function(req, res){
+	var id = req.params.id;
+	delete req.body._id;
+	db.event.updateById(id, req.body, function(err, result){
+		if (err) res.send("error");
+		else res.send(req.body);
+	});			
+});
+app.delete('/events/:id', function(req, res){
+	var id = req.params.id;
+	db.event.removeById(id, function(err, result){
+		if (err) res.send("error");
+		else res.send(req.body);
+	});
+});
+
 
 app.listen(3000);
